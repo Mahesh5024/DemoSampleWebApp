de7f4919f67a28d35b48746ce033671742b03558
Mahesh5024 <98993359+Mahesh5024@users.noreply.github.com>
2022-02-07 09:43:12 -0600

    Add files via upload

---
+++ DemoSampleWebApp-master/DemoSampleWebApp/wwwroot/Scripts/Crud.js
@@ -0,0 +1,45 @@
+﻿var newapp = angular.module('newApp', ['ui.grid']);
+
+newapp.controller('newController', function ($scope, $http) {
+    $scope.myData = [];
+
+    getData();
+
+    function getData()
+    {
+        var request = {
+            method: 'GET',
+            url: 'https://demoapi-2-dev-as.azurewebsites.net/API/Persons',
+            dataType: 'json',
+            headers:
+            {
+                'Content-Type': 'application/json',
+                'api-version': '1.0'
+            }
+        };
+
+        $http(request).then(function (response) {
+            console.log('Get Response', response);           
+            console.log('Get Response', response.data); 
+            //var jsondataobj = JSON.parse(response);
+            //console.log('JSON', jsondataobj);  
+            //var rowcount = jsondataobj.length;
+            //console.log('Count', rowcount);  
+
+            response.data.forEach(function (row) {
+                $scope.myData.push(row);
+            });                  
+            
+            $scope.gridData.data = [];    
+            $scope.gridData.data = $scope.myData;
+
+        });
+
+    }
+});
+
+
+        
+
+
+    
---
+++ DemoSampleWebApp-master/DemoSampleWebApp/wwwroot/Scripts/MyAngular.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('myapp', []);
+
+app.controller("hellocontroller", function ($scope,$http) {
+    $scope.Name = 'Test Name';
+    $scope.content = 'Test Content';
+
+    $scope.callwebapi = function () {
+        var request =
+        {   method: 'GET',
+            url: 'http://localhost:50282/api/Persons',
+            headers:
+            {
+                'Content-Type': 'application/json',
+                'api-version': '2.0'
+            }
+        };
+
+        $http(request).then(function (response)
+        {
+            $scope.data = JSON.parse(response);            
+        },function (error)
+          {
+            console.log(error);
+
+          });
+    };
+    
+});
+
